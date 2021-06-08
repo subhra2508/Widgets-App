138e88b1c9edd47b3fe2d9a6550c65a1a6abd024
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-06-08 23:32:58 +0530

    added searchbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Accordion from './components/Accordion';
-
+import Search from './components/Search';
 const items = [
     {
       title:'What is React?',
@@ -21,7 +21,7 @@ const App = () => {
     return (
         <div>
             <br/>
-            <Accordion items={items}/>
+             <Search/>
         </div>
     )
 };
