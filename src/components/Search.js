ede7170d74c12bbe5d4a9bf6046f19068ff1b3e1
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-06-11 23:21:17 +0530

    added search

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,38 +3,72 @@ import axios from 'axios';
 
 const Search = () => {
     const [term,setTerm ] = useState('programming');
+    const [debouncedTerm,setDebouncedTerm] = useState(term);
     const [results,SetResults] = useState([]);
-    console.log(results)
+    // console.log(results)
+
     useEffect(() => {
-      const search = async () => {
-          const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
-              params:{
-                  action:'query',
-                  list:'search',
-                  origin:'*',
-                  format:'json',
-                  srsearch:term
-              }
-          });
-          SetResults(data.query.search);
-      };
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        },1000);
+        return () => {
+            clearTimeout(timerId);
+        }
+    },[term]);
 
-      if(term && !results.length){
-         search(); 
-      }
-      else{
+    useEffect(() => {
+        const search = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+                params:{
+                    action:'query',
+                    list:'search',
+                    origin:'*',
+                    format:'json',
+                    srsearch:debouncedTerm
+                }
+            });
+            SetResults(data.query.search);
+        };
         
-     const timeoutId = setTimeout(() => {
-        if(term){
+        if(debouncedTerm){
             search();
-          }
-      },500);
+        }
 
-      return () => {
-          clearTimeout(timeoutId)
-      }
-     }
-    },[term])
+    },[debouncedTerm]);
+
+
+
+
+    // useEffect(() => {
+    //     const search = async () => {
+    //         const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+    //             params:{
+    //                 action:'query',
+    //                 list:'search',
+    //                 origin:'*',
+    //                 format:'json',
+    //                 srsearch:term
+    //             }
+    //         });
+    //         SetResults(data.query.search);
+    //     };
+  
+    //   if(term && !results.length){
+    //      search(); 
+    //   }
+    //   else{
+        
+    //  const timeoutId = setTimeout(() => {
+    //     if(term){
+    //         search();
+    //       }
+    //   },500);
+
+    //   return () => {
+    //       clearTimeout(timeoutId)
+    //   }
+    //  }
+    // },[term,results.length])
 
     const renderedResults = results.map((result) => {
         return (
