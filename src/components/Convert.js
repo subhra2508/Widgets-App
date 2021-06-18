bd79ec5c4f7871921cac7885f417bcde7b096ab9
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-06-18 22:49:06 +0530

    initial commit

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,12 +1,37 @@
+import axios from 'axios';
 import React,{useEffect,useState} from 'react';
 
 
 const Convert = ({language,text}) => {
+    const [translated,setTranslated] = useState('')
+    const [debouncedText,setDebouncedText] = useState(text);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text)
+        },500)
+        return () => {
+            clearTimeout(timerId)
+        }
+    },[text])
     useEffect(()=>{
-        console.log('new language text')
-    },[language,text])
+        // console.log('new language text')
+        const doTranslation = async () => {
+            const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2',{},{
+            params:{
+                q:debouncedText,
+                target:language.value,
+                key:'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+            }
+        });
+        setTranslated(data.data.translations[0].translatedText)
+        }
+        doTranslation();
+    },[language,debouncedText])
     return (
-        <div></div>
+        <div>
+            <h1 className="ui header">{translated}</h1>
+        </div>
     )
 }
 
